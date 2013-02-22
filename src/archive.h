1e4299baad922e3b56032c0c37ed8100a15ace69
NikSaak <niksaak@gmail.com>
2013-02-22 19:05:39 +0200

    more architecture fixes

--- src/archive.h
+++ src/archive.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <inttypes.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -12,25 +10,26 @@ typedef enum aie_ArcFormatKind { // archive kinds enumeration
 } aie_ArcFormatKind;
 
 typedef enum aie_ArcFormatStatus {
-  NORMAL, // normal format
+  NORMAL, // everything OK with this one
   UNSAFE, // rtfm recomended
   EXPERIMENTAL, // result is not guaranteed
   HACK, // dirty, prolly not for general use
   WRITEONLY, // packing only
-  PLACEHOLDER, // but may be of use~
-  READONLY // unpacking only
+  READONLY, // unpacking only
+  PLACEHOLDER = 0xF0000000; // placeholder, but may be of use
 } aie_ArcFormatStatus;
 
-typedef struct aie_ArcFile (*aie_ArcOpenFun)();
-typedef struct aie_ArcFile (*aie_ArcCreateFun)();
+typedef struct aie_ArcFile (*aie_ArcOpenFun)(const char* name);
+typedef struct aie_ArcFile (*aie_ArcCreateFun)(const char* name);
 typedef bool (*aie_ArcExtractFun)();
 
 typedef struct aie_ArcFormat { // Struct with archive format description
   enum aie_ArcFormatKind id; // identifier.
   const char* name; // archiver name
-  const char** extensions; // pointer to array with file extensions for archive
+  char*const* arc_ext; // pointer to array with file extensions for archive
+  char*const* meta_ext; // ~~~ with file extensions for metadata
   enum aie_ArcFormatStatus status;
-  unsigned filename_len;
+  unsigned filename_len; // max filename len
   unsigned drv_version; // version in format 0xYYYYmmdd
 
   aie_ArcOpenFun open;
@@ -40,7 +39,7 @@ typedef struct aie_ArcFormat { // Struct with archive format description
 } aie_ArcFormat;
 
 typedef struct aie_ArcAllocTableSegment {
-  // File segmentation info. If 'next' == NULL, file is one-piece
+  // File segmentation info.
   size_t offset;
   size_t size;
   struct aie_ArcAllocTableSegment* next;
@@ -59,7 +58,9 @@ typedef struct aie_ArcAllocTable {
 } aie_ArcAllocTable;
 
 typedef struct aie_ArcFile {
+  // Archive file
   const struct aie_ArcFormat* fmt; // pointer to archive format info
+  unsigned subtype; // file subtype
   struct aie_ArcAllocTable table; // unified allocation table for archive
   FILE* file; // file descriptor
   char* name; // file name (prolly optional)
