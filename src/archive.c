7b5bf1e09f341828eeb61bb4144f919e76cd9d5b
NikSaak <niksaak@gmail.com>
2013-02-23 12:09:20 +0200

    added get/set-ers for arcFormat

--- src/archive.c
+++ src/archive.c
@@ -1,10 +1,94 @@
+#include <string.h>
+
 #include "archive.h"
 
-aie_ArcFormat aie_arcfmt(aie_ArcFormatKind kind)
+const aie_ArcFormat* aie_arcfmt(aie_ArcFormatKind kind)
 { // placeholder
-  static aie_ArcFormat fmt;
+  static aie_ArcFormat unsup = 
+  { aie_ARC_UNSUPPORTED,
+    "UNSUPPORTED FORMAT",
+    NULL,
+    NULL,
+    aie_PLACEHOLDER | aie_HACK,
+    0,
+    0,
+    NULL,
+    NULL,
+    NULL
+  };
 
-  if(kind == aie_ARC_UNSUPPORTED)
-    fmt.name = "unsupported";
-  return fmt;
+  switch(kind)
+  {
+    case aie_ARC_UNSUPPORTED:
+    default:
+      return &unsup;
+  }
+  return NULL;
+}
+
+const char* aie_arcfmt_name(const aie_ArcFormat* format)
+{
+  return format->name;
+}
+
+const char* aie_arcfmt_extensions(const aie_ArcFormat* format)
+{
+  static char extensions[512] = {0};
+
+  strncat(extensions, format->arc_ext, sizeof extensions - 1);
+  sprintf(extensions, " ");
+  strncat(extensions, format->meta_ext, 
+          sizeof extensions - strlen(extensions));
+
+  return extensions;
+}
+
+aie_ArcFormatStatus aie_arcfmt_status(const aie_ArcFormat* format)
+{
+  return format->status;
+}
+
+const char* aie_arcfmt_statusstr(const aie_ArcFormat* format)
+{
+  aie_ArcFormatStatus status = aie_arcfmt_status(format);
+  static char ret[512] = {0}
+  char* str = NULL;
+
+  switch(status & 0x7FFFFFFF)
+  {
+    case aie_NORMAL:
+      str = "Normal";
+      break;
+    case aie_UNSAFE:
+      str = "Unsafe";
+      break;
+    case aie_EXPERIMENTAL:
+      str = "Experimental";
+      break;
+    case aie_HACK:
+      str = "Hack";
+      break;
+    case aie_WRITEONLY:
+      str = "Write only";
+      break;
+    case aie_READONLY:
+      str = "Read only";
+      break;
+  }
+
+  snprintf(ret, sizeof ret, str);
+  if(status & aie_PLACEHOLDER)
+    snprintf(ret, sizeof ret - strlen(ret) - 1, ", Placeholder");
+
+  return ret;
+}
+
+size_t aie_arcfmt_namelen(const aie_ArcFormat* format)
+{
+  return format->filename_len;
+}
+
+uint32_t aie_arcfmt_ver(const aie_ArcFormat* format)
+{
+  return format->drv_version;
 }
--- src/archive.h
+++ src/archive.h
@@ -1,36 +1,40 @@
 #pragma once
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdbool.h>
 
 // Functions:
 
-typedef enum aie_ArcFormatKind { // archive kinds enumeration
+typedef enum aie_ArcFormatKind
+{ // archive kinds enumeration
   aie_ARC_UNSUPPORTED
 } aie_ArcFormatKind;
 
-typedef enum aie_ArcFormatStatus {
-  NORMAL, // everything OK with this one
-  UNSAFE, // rtfm recomended
-  EXPERIMENTAL, // result is not guaranteed
-  HACK, // dirty, prolly not for general use
-  WRITEONLY, // packing only
-  READONLY, // unpacking only
-  PLACEHOLDER = 0xF0000000; // placeholder, but may be of use
+typedef enum aie_ArcFormatStatus
+{
+  aie_NORMAL, // everything OK with this one
+  aie_UNSAFE, // rtfm recomended
+  aie_EXPERIMENTAL, // result is not guaranteed
+  aie_HACK, // dirty, prolly not for general use
+  aie_WRITEONLY, // packing only
+  aie_READONLY, // unpacking only
+  aie_PLACEHOLDER = 0x80000000 // placeholder, but may be of use
 } aie_ArcFormatStatus;
 
 typedef struct aie_ArcFile (*aie_ArcOpenFun)(const char* name);
 typedef struct aie_ArcFile (*aie_ArcCreateFun)(const char* name);
 typedef bool (*aie_ArcExtractFun)();
 
-typedef struct aie_ArcFormat { // Struct with archive format description
+typedef struct aie_ArcFormat
+{ // Struct with archive format description
   enum aie_ArcFormatKind id; // identifier.
   const char* name; // archiver name
-  char*const* arc_ext; // pointer to array with file extensions for archive
-  char*const* meta_ext; // ~~~ with file extensions for metadata
+  const char* arc_ext; // file extensions for archive, space separated
+  const char* meta_ext; // file extensions for metadata, space separated
   enum aie_ArcFormatStatus status;
-  unsigned filename_len; // max filename len
-  unsigned drv_version; // version in format 0xYYYYmmdd
+  size_t filename_len; // max filename len
+  uint32_t drv_version; // version in format 0xYYYYmmdd
 
   aie_ArcOpenFun open;
   aie_ArcCreateFun create;
@@ -38,37 +42,60 @@ typedef struct aie_ArcFormat { // Struct with archive format description
 
 } aie_ArcFormat;
 
-typedef struct aie_ArcAllocTableSegment {
-  // File segmentation info.
+typedef struct aie_ArcAllocSegment
+{ // Archived file segmentation info.
   size_t offset;
   size_t size;
-  struct aie_ArcAllocTableSegment* next;
-} aie_ArcAllocTableSegment;
+  struct aie_ArcAllocSegment* next;
+} aie_ArcAllocSegment;
 
-typedef struct aie_ArcAllocTable {
-  // Archive allocaton table
+typedef struct aie_ArcAlloc
+{ // Archive allocaton unit
   char* name; // filename
-  struct aie_ArcAllocTableSegment* segments; // file segmentation
+  struct aie_ArcAllocSegment* segments; // file segmentation
   unsigned size; // uncompressed size
   bool compressed;
   bool encrypted;
-  void* data; // optional data
 
-  struct aie_ArcAllocTable* next;
-} aie_ArcAllocTable;
+  struct aie_ArcAlloc* next;
+} aie_ArcAlloc;
 
-typedef struct aie_ArcFile {
-  // Archive file
-  const struct aie_ArcFormat* fmt; // pointer to archive format info
-  unsigned subtype; // file subtype
-  struct aie_ArcAllocTable table; // unified allocation table for archive
+typedef struct aie_ArcFile
+{ // Archive file
   FILE* file; // file descriptor
-  char* name; // file name (prolly optional)
-
-  struct aie_ArcFile* next; // pointer to next part of archive
+  char* name; // filename including path
+  unsigned subtype; // file subtype
+  struct aie_ArcFile* next;
 } aie_ArcFile;
+
+typedef struct aie_Archive
+{ // Archive abstraction
+  const struct aie_ArcFormat* fmt; // pointer to archive format info
+  struct aie_ArcAlloc* table; // unified allocation table for archive
+  struct aie_ArcFile* files; // files of archive
+} aie_Archive;
 
 // Functions:
 
-extern aie_ArcFormat aie_arcfmt(aie_ArcFormatKind kind);
+extern const aie_ArcFormat* aie_arcfmt(aie_ArcFormatKind kind);
 // get format for archives of kind
+
+extern const char* aie_arcfmt_name(const aie_ArcFormat* format);
+// get format name
+
+extern const char* aie_arcfmt_extensions(const aie_ArcFormat* format);
+// get string, listing acceptable fileextensions for format, space separated.
+// WARNING: returns static string which is modified on each call
+
+extern aie_ArcFormatStatus aie_arcfmt_status(const aie_ArcFormat* format);
+// get status of a formatter
+
+extern const char* aie_arcfmt_statusstr(const aie_ArcFormat* format);
+// get string representation for status of a formatter
+// WARNING: returns static string which is modified on each call
+
+extern size_t aie_arcfmt_namelen(const aie_ArcFormat* format);
+// get maximum filename length in bytes for format
+
+extern uint32_t aie_arcfmt_ver(const aie_ArcFormat* format);
+// get formatter version
